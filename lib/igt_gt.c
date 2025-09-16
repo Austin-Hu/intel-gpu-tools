psennats/drm.igt-gpu-tools

commit 114b9ff31d1974b198c34c99759124c14afdff3c

lib/igt_gt: Add igt_open_forcewake_handle_for_pcidev()
In several places we are getting the pci_dev for an fd and then passing
both the pci_dev and the fd to intel_register_access_init(). Add a
helper so intel_register_access_init() can operate on pci_dev only.

v2:
- Use hexa for BDF
- Consistently return -errno on error

Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
Link: https://lore.kernel.org/r/20240926041651.412396-3-lucas.demarchi@intel.com
Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

lib/igt_gt.c
@@ -30,6 +30,7 @@
 #include <sys/stat.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
+#include <pciaccess.h>
 
 #include "drmtest.h"
 #include "i915/gem.h"
@@ -512,6 +513,45 @@ int igt_open_forcewake_handle(int fd)
 		fn = "i915_forcewake_user";
 
 	return igt_debugfs_open(fd, fn, O_RDONLY);
+}
+
+int igt_open_forcewake_handle_for_pcidev(const struct pci_device *pci_dev)
+{
+	static const char *FORCEWAKE_FILES[] = {
+		"forcewake_all", "i915_forcewake_user",
+		NULL
+	};
+	const char *debugfs_root = igt_debugfs_mount(), **fn;
+	char path[PATH_MAX];
+	int dirlen, ret;
+
+	if (!debugfs_root)
+		return -ENOENT;
+
+	ret = snprintf(path, sizeof(path), "%s/dri/%04x:%02x:%02x.%x",
+		       debugfs_root,
+		       pci_dev->domain, pci_dev->bus, pci_dev->dev,
+		       pci_dev->func);
+	if (ret < 0 || ret >= sizeof(path))
+		return -EINVAL;
+
+	dirlen = ret;
+	for (fn = FORCEWAKE_FILES; *fn; fn++) {
+		ret = snprintf(path + dirlen, sizeof(path) - dirlen,
+			       "/%s", *fn);
+		if (ret < 0 || ret >= sizeof(path) - dirlen)
+			return -EINVAL;
+
+		ret = open(path, O_RDONLY);
+		if (ret >= 0)
+			return ret;
+
+		if (errno != ENOENT)
+			return -errno;
+	}
+
+	return -ENOENT;
+
 }
 
 #if defined(__x86_64__) || defined(__i386__)

lib/igt_gt.h
@@ -31,6 +31,8 @@
 #include "i915/i915_drm_local.h"
 #include "i915_drm.h"
 
+struct pci_device;
+
 void igt_require_hang_ring(int fd, uint32_t ctx, int ring);
 
 typedef struct igt_hang {
@@ -63,6 +65,7 @@ void igt_fork_hang_helper(void);
 void igt_stop_hang_helper(void);
 
 int igt_open_forcewake_handle(int fd);
+int igt_open_forcewake_handle_for_pcidev(const struct pci_device *pci_dev);
 
 int igt_setup_clflush(void);
 void igt_clflush_range(void *addr, int size);

Austin-Hu/intel-gpu-tools

commit 98843bf57905c2d8c8ccd8fe5050909cc95b8fbd

lib/gt: Fix IGT_NO_FORCEWAKE handling
igt_open_forcewake_handle_for_pcidev() forgets to check for
the IGT_NO_FORCEWAKE environment variable, which is a real
problem when one wants to frob the registers without forcing
power state changes in the hardware (eg. to observe display
low power watermark residencies/etc.)

Add the missing IGT_NO_FORCEWAKE check.

Cc: Lucas De Marchi <lucas.demarchi@intel.com>
Fixes: 114b9ff31d19 ("lib/igt_gt: Add igt_open_forcewake_handle_for_pcidev()")
Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

lib/igt_gt.c
@@ -528,6 +528,9 @@ int igt_open_forcewake_handle_for_pcidev(const struct pci_device *pci_dev)
 	char path[PATH_MAX];
 	int dirlen, ret;
 
+	if (getenv("IGT_NO_FORCEWAKE"))
+		return -1;
+
 	if (!debugfs_root)
 		return -ENOENT;
 