a9f9639948a0a79377fea7b27a64502a8208a1a0
Austin Hu <austin.hu@intel.com>
2026-07-01 22:54:35 -0700

    kms_scaler: updated the case with TEST_ONLY checking.
    Signed-off-by: Austin Hu <austin.hu@intel.com>

--- tests/kms_scaler.c
+++ tests/kms_scaler.c
@@ -51,9 +51,15 @@ static void test(data_t *data)
 
 	plane = igt_output_get_plane_type(data->output,
 					    DRM_PLANE_TYPE_PRIMARY);
+	igt_plane_set_size(plane, dw, dh);
+
 	igt_plane_set_fb(plane, &fb);
 
 	igt_fb_set_size(&fb, plane, sw, sh);
+
+	// Prepare the CRTC (DPMS on it) before testing.
+	igt_display_commit_atomic(&data->display, DRM_MODE_ATOMIC_ALLOW_MODESET,
+			NULL);
 
 	while (dw >= 8 && dh >= 8) {
 		float hscale = (float)sw/dw;
@@ -63,13 +69,13 @@ static void test(data_t *data)
 
 		igt_reset_fifo_underrun_reporting(data->drm_fd);
 		if (!igt_display_try_commit_atomic(&data->display,
-					flags | DRM_MODE_ATOMIC_TEST_ONLY, NULL)) {
+					DRM_MODE_ATOMIC_TEST_ONLY, NULL)) {
 			igt_display_commit_atomic(&data->display, flags, NULL);
 
 			igt_info("scale %f %f (rate %d)\n", hscale, vscale,
 					(int)(mode->clock * hscale * vscale / 2.0f));
 		} else
-			igt_info("scale %f %f (rate %d) atomic test failed.\n", hscale,
+			igt_warn("scale %f %f (rate %d) atomic TEST failed.\n", hscale,
 					vscale, (int)(mode->clock * hscale * vscale / 2.0f));
 
 		dw -= 4;
